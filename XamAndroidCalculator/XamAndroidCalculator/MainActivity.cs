5c9c7ef4a21c17b02dbb802f79a2810c04ef5a7f
DESKTOP-6ICSE8H\cddat <hareshdc@gmail.com>
2018-08-24 18:35:46 -0700

    main logic

--- XamAndroidCalculator/XamAndroidCalculator/MainActivity.cs
+++ XamAndroidCalculator/XamAndroidCalculator/MainActivity.cs
@@ -9,15 +9,43 @@ using Android.Views;
 namespace XamAndroidCalculator
 {
 	[Activity(Label = "@string/app_name", Theme = "@style/AppTheme.NoActionBar", MainLauncher = true)]
-	public class MainActivity : AppCompatActivity
+	public class MainActivity : Activity
 	{
+        Button btnCalculate;
+        EditText etInput;
+        TextView tvTip;
+        TextView tvTotal;
 
-		protected override void OnCreate(Bundle savedInstanceState)
+        protected override void OnCreate(Bundle savedInstanceState)
 		{
 			base.OnCreate(savedInstanceState);
 
 			SetContentView(Resource.Layout.Main);
-		}
-	}
+
+            btnCalculate = FindViewById<Button>(Resource.Id.btnCalculate);
+            etInput = FindViewById<EditText>(Resource.Id.etInput);
+            tvTip = FindViewById<TextView>(Resource.Id.tvTipVal);
+            tvTotal = FindViewById<TextView>(Resource.Id.tvTotalVal);
+
+            btnCalculate.Click += BtnCalculate_Click;
+        }
+
+        private void BtnCalculate_Click(object sender, EventArgs e)
+        {
+            string text = etInput.Text;
+
+            double bill;
+
+            if (Double.TryParse(text, out bill))
+            {
+                double tip = bill * 0.15;
+                Console.Write(tip.ToString());
+                double total = bill + tip;
+
+                tvTip.Text = tip.ToString();
+                tvTotal.Text = total.ToString();
+            }
+        }
+    }
 }
 
